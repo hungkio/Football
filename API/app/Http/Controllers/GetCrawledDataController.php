6a06dae8d930b30149f89f00e33024a7dc28a43f
PikeTheFreelancer <pikefreeman1997@gmail.com>
2024-06-23 16:20:17 +0700

    fixture api

--- API/app/Http/Controllers/GetCrawledDataController.php
+++ API/app/Http/Controllers/GetCrawledDataController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\GetFixturesRequest;
+use App\Models\Fixture;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -10,21 +11,12 @@ class GetCrawledDataController extends Controller
 {
     public function getFixtures(GetFixturesRequest $request){
         $date = $request->date;
-        $filePath = 'Crawl/database/crawls/' . $date . '-fixture.json';
-        
-        if (Storage::exists($filePath)) {
-            dd('run here');
-            $contents = Storage::get($filePath);
-
-            $data = json_decode($contents, true);
-
-            if (json_last_error() === JSON_ERROR_NONE) {
-                return $data;
-            } else {
-                return [];
-            }
-        } else {
-            return [];
+        $fixtures = Fixture::where('fixture->date','like', $date.'%')->get();
+        $arr = [];
+        foreach ($fixtures as $fixture) {
+            $leagueName = $fixture->league['name'];
+            $arr[$leagueName][] = $fixture->toArray();
         }
+        return response()->json($arr);
     }
 }
