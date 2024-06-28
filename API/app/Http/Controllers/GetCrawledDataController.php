e80bc3da43ffe48eb5076e61758283d6cd9cc756
PikeTheFreelancer <pikefreeman1997@gmail.com>
2024-06-28 22:43:25 +0700

    update live fixtures

--- API/app/Http/Controllers/GetCrawledDataController.php
+++ API/app/Http/Controllers/GetCrawledDataController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\GetFixturesRequest;
 use App\Models\Fixture;
+use App\Models\LiveFixture;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -21,7 +22,7 @@ class GetCrawledDataController extends Controller
     }
 
     public function getLiveFixtures(){
-        $fixtures = Fixture::all();
+        $fixtures = LiveFixture::all();
         $arr = [];
         foreach ($fixtures as $fixture) {
             $leagueName = $fixture->league['name'];
