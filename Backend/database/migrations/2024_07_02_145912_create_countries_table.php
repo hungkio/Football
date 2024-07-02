8df5f2e1f0e761ddfc21d6ab9066fac9feba55ff
PikeTheFreelancer <pikefreeman1997@gmail.com>
2024-07-02 22:33:38 +0700

    crawl countries

---
+++ Backend/database/migrations/2024_07_02_145912_create_countries_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->integer('api_id')->nullable();
+            $table->string('name')->nullable();
+            $table->string('code')->nullable();
+            $table->string('flag')->nullable();
+            $table->boolean('from_team')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('countries');
+    }
+};
