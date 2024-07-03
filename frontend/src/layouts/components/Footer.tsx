6543f3b6c8fd99843b75a5e5a05110a0191006d6
TDevilPhan <phantuyen.dev@gmail.com>
2024-07-03 17:42:45 +0700

    Add footer

--- frontend/src/layouts/components/Footer.tsx
+++ frontend/src/layouts/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="w-full py-[50px] pb-[180px] bg-black">
       <div className="container mx-auto text-xs text-white">
-        <p className="my-3">
+        <p className="my-3 text-sm">
           <strong>Partners: </strong>
           <Link className="text-primary hover:text-red border-r border-secondary px-1 last:border-none last:pr-0" to={''}>
             Clothes & Gifts Printeval
@@ -18,7 +18,7 @@ const Footer = () => {
           </Link>
         </p>
         <div className="mb-4">
-          <p className="text-center mb-4">Nhận dự đoán bóng đá mới nhất trong hộp thư của bạn! Đừng đăng ký nếu bạn chưa 18</p>
+          <p className="text-center mb-4">Nhận dự đoán bóng đá mới nhất trong hộp thư của bạn! Vui lòng không đăng ký nếu bạn chưa đủ 18 tuổi</p>
           <div className="flex justify-center">
             <input className="border-2 rounded-tl-[32px] rounded-bl-[32px] p-4 w-[400px]" placeholder="Enter your email address" type="text" />
             <button className="p-4 rounded-tr-[32px] rounded-br-[32px] bg-primary border-2 border-primary">Đăng ký</button>
