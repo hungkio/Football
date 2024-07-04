aeb940df758a357d9568655cc4dceb34601a871b
pikeTheDev <nghiavt2@rikkeisoft.com>
2024-07-04 14:33:32 +0700

    crawl seasons data

--- Crawl/app/Services/ApiService.php
+++ Crawl/app/Services/ApiService.php
@@ -129,4 +129,21 @@ class ApiService
         return null;
     }
 
+    public function crawlSeasons()
+    {
+        $response = $this->client->request('GET', 'leagues/seasons', [
+            'headers' => [
+                'X-RapidAPI-Key' => config('app.rapid_api_key'),
+                'X-RapidAPI-Host' => 'api-football-v1.p.rapidapi.com',
+                'Accept'        => 'application/json',
+            ]
+        ]);
+
+        if ($response->getStatusCode() == 200) {
+            return json_decode($response->getBody(), true);
+        }
+
+        return null;
+    }
+
 }
