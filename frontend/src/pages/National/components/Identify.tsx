c4c835bc7ed36cf7f57a18896084d0cc8e297b1f
TDevilPhan <phantuyen.dev@gmail.com>
2024-07-15 17:21:49 +0700

    National page

--- frontend/src/pages/National/components/Identify.tsx
+++ frontend/src/pages/National/components/Identify.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Identify = () => {
   return (
     <div>
-      <div>
+      <div className="text-xs p-2.5 bg-[#f6fbff] my-2">
         <p>
           Nhận định bóng đá Anh hôm nay và ngày mai: Dự đoán kết quả tỷ số bóng đá Anh các trận thi đấu tối và đêm nay. Cập nhật tỷ lệ, soi kèo, nhận định, dự
           đoán đội hình dự kiến nhanh và chính xác nhất các giải bóng đá hàng đầu của Anh bao gồm: giải VĐQG Anh - Hạng nhất H1 - Hạng 2 H2 - Hạng 3 H3 Anh.
@@ -15,7 +15,33 @@ const Identify = () => {
           số bàn thắng, đội vô địch và tỷ số hiệp 1-cả trận.
         </p>
       </div>
-      <div className="pb-5 mb-5 border border-[#eee] flex gap-4">
+      <div className="pb-5 mb-5 border-b border-[#eee] flex gap-4">
+        <img src="https://cdn.kqbd.mobi/w150/26.05.2024/nhan-dinh-leeds-united-vs-southampton-21h00-ngay-26-5_1716687132.jpg?v=1716687259" alt="" />
+        <div>
+          <Link className="text-primary hover:text-red font-bold" to={''}>
+            Nhận định Leeds United vs Southampton, 21h00 ngày 26/5
+          </Link>
+          <p className="text-xs mb-2.5">Minh Long - 26/05/2024 08:32</p>
+          <p className="text-xs">
+            Nhận định Leeds United vs Southampton, dự đoán bóng đá hạng nhất Anh hôm nay 21h00 ngày 26/5 chính xác. Nhận định bóng đá Anh: dự đoán kết quả tỷ số
+            trận chung kết play-off thăng hạng tối và đêm nay nhanh nhất.
+          </p>
+        </div>
+      </div>
+      <div className="pb-5 mb-5 border-b border-[#eee] flex gap-4">
+        <img src="https://cdn.kqbd.mobi/w150/26.05.2024/nhan-dinh-leeds-united-vs-southampton-21h00-ngay-26-5_1716687132.jpg?v=1716687259" alt="" />
+        <div>
+          <Link className="text-primary hover:text-red font-bold" to={''}>
+            Nhận định Leeds United vs Southampton, 21h00 ngày 26/5
+          </Link>
+          <p className="text-xs mb-2.5">Minh Long - 26/05/2024 08:32</p>
+          <p className="text-xs">
+            Nhận định Leeds United vs Southampton, dự đoán bóng đá hạng nhất Anh hôm nay 21h00 ngày 26/5 chính xác. Nhận định bóng đá Anh: dự đoán kết quả tỷ số
+            trận chung kết play-off thăng hạng tối và đêm nay nhanh nhất.
+          </p>
+        </div>
+      </div>
+      <div className="pb-5 mb-5 border-b border-[#eee] flex gap-4">
         <img src="https://cdn.kqbd.mobi/w150/26.05.2024/nhan-dinh-leeds-united-vs-southampton-21h00-ngay-26-5_1716687132.jpg?v=1716687259" alt="" />
         <div>
           <Link className="text-primary hover:text-red font-bold" to={''}>
