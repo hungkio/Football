03c4acafb49ac63f0dfcabda818136859ba0cad2
pikeTheDev <nghiavt2@rikkeisoft.com>
2024-07-23 19:28:26 +0700

    update migration

--- Backend/database/migrations/2024_07_23_163935_create_tags_table.php
+++ Backend/database/migrations/2024_07_23_163935_create_tags_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('tag');
+            $table->string('tag_slug');
             $table->timestamps();
         });
     }
