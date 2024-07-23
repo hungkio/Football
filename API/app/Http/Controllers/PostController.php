689cfe042561f814aac8682a75e7fbe3f49fc2d3
pikeTheDev <nghiavt2@rikkeisoft.com>
2024-07-23 15:31:15 +0700

    api get post by id

--- API/app/Http/Controllers/PostController.php
+++ API/app/Http/Controllers/PostController.php
@@ -2,16 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\GetPostByIdRequest;
 use App\Http\Requests\GetPostsByCategoryRequest;
 use App\Http\Requests\GetPostsOnpageRequest;
 use App\Models\Post;
 use App\Models\Taxonable;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
     public function getPostsOnPage(GetPostsOnpageRequest $request){
-        $posts = Post::whereJsonContains('on_pages', $request->page_id)->paginate($request->per_page);
+        $posts = Post::whereJsonContains('on_pages', $request->page_id)
+        ->when($request->date, function($query) use ($request){
+            $query->where('created_at', $request->date);
+        })
+        ->paginate($request->per_page);
         
         return response()->json([
             'status' => true,
@@ -28,7 +34,11 @@ class PostController extends Controller
             foreach ($taxonables as $taxonable) {
                 $postIds[] = $taxonable->taxonable_id;
             }
-            $posts = Post::whereIn('id', $postIds)->paginate($request->per_page);
+            $posts = Post::whereIn('id', $postIds)
+            ->when($request->date, function($query) use ($request){
+                $query->where('created_at', $request->date);
+            })
+            ->paginate($request->per_page);
     
             return response()->json([
                 'status' => true,
@@ -38,6 +48,21 @@ class PostController extends Controller
             return response()->json([
                 'status' => false,
                 'message' => $th
+            ]);
+        }
+    }
+
+    public function getPostById(GetPostByIdRequest $request){
+        try {
+            $post = Post::find($request->post_id);
+            return response()->json([
+                'status' => true,
+                'data' => $post,
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th,
             ]);
         }
     }
--- API/routes/api.php
+++ API/routes/api.php
@@ -31,5 +31,6 @@ Route::get('live-fixtures', [GetCrawledDataController::class, 'getLiveFixtures']
 Route::get('get-menus', [MenuController::class, 'getAll']);
 Route::get('posts', [PostController::class, 'getPostsOnPage']);
 Route::get('getPostsByCategory', [PostController::class, 'getPostsByCategory']);
+Route::get('getPostById', [PostController::class, 'getPostById']);
 Route::get('comments', [CommentController::class, 'getCommentsFromPost']);
 Route::get('categories', [CategoryController::class, 'getPostCategories']);
