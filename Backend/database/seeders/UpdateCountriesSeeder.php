322000ce2813e3a40a120bf6a490cadc082b8b30
Vương Trần <v2.vnit@gmail.com>
2024-07-28 11:40:57 +0700

    update Region for Country

---
+++ Backend/database/seeders/UpdateCountriesSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Domain\Country\Models\Country;
+
+class UpdateCountriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $json = json_decode(file_get_contents('https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/countries.json'), true);
+        foreach($json as $key => $item){
+            $code = $item['iso2'];
+            $region = $item['region'];
+            $region_id = $item['region_id'];
+            $subregion = $item['subregion'];
+            $subregion_id = $item['subregion_id'];
+            Country::where('code',$code)->update(['region'=>$region, 'region_id'=>$region_id, 'subregion'=>$subregion, 'subregion_id'=>$subregion_id]);
+        }
+    }
+}
