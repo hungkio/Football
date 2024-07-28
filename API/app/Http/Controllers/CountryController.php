59580da05cfd505dd71cdd352a5933789e380e09
Vương Trần <v2.vnit@gmail.com>
2024-07-28 12:25:21 +0700

    debug country api

--- API/app/Http/Controllers/CountryController.php
+++ API/app/Http/Controllers/CountryController.php
@@ -9,10 +9,13 @@ class CountryController extends Controller
 {
     public function index(Request $request){
         try {
-            $countries = Country::when($request->keyword, function($query) use ($request){
-                $query->where('name','like', $request->keyword . '%')
+            $countries = Country::select('countries.*, regions.name_vi region_vi, subregion.name_vi subregion_vi')::when($request->keyword, function($query) use ($request){
+                $query->where('name', 'like', $request->keyword . '%')
                       ->orWhere('code', 'like', $request->keyword . '%');
-            })->paginate($request->per_page);
+            })
+            ->join('regions','countries.region_id','=','regions.id')
+            ->join('subregion','countries.subregion_id','=','subregion.id')
+            ->paginate($request->per_page);
             return response()->json([
                 'status' => true,
                 'data' => $countries
