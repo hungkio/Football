0ce16173946a5b8bd57580a19c1cf5dd4ff1f473
Vương Trần <v2.vnit@gmail.com>
2024-07-29 23:40:14 +0700

    debug

--- Backend/resources/views/admin/layouts/base.blade.php
+++ Backend/resources/views/admin/layouts/base.blade.php
@@ -63,32 +63,32 @@
     if($('.noidung').length>0){
         console.log('check');
         tinymce.init({
-    selector: "textarea.noidung",
-    plugins: [
-        "image,link,file-manager",
-    ],
-    Flmngr: {
-        apiKey: "FLMNFLMN",
-    },
-    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | image",
-    file_picker_callback: function (callback, value, meta) {
-        let x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
-        let y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+            selector: "textarea.noidung",
+            plugins: [
+                "image",
+            ],
+            Flmngr: {
+                apiKey: "FLMNFLMN",
+            },
+            toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | image",
+            file_picker_callback: function (callback, value, meta) {
+                let x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+                let y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
 
-        let type = 'image' === meta.filetype ? 'Images' : 'Files',
-            url  = '/laravel-filemanager?editor=tinymce5&type=' + type;
+                let type = 'image' === meta.filetype ? 'Images' : 'Files',
+                    url  = '/laravel-filemanager?editor=tinymce5&type=' + type;
 
-        tinymce.activeEditor.windowManager.openUrl({
-            url : url,
-            title : 'Filemanager',
-            width : x * 0.8,
-            height : y * 0.8,
-            onMessage: (api, message) => {
-                callback(message.content);
+                tinymce.activeEditor.windowManager.openUrl({
+                    url : url,
+                    title : 'Filemanager',
+                    width : x * 0.8,
+                    height : y * 0.8,
+                    onMessage: (api, message) => {
+                        callback(message.content);
+                    }
+                });
             }
         });
-    }
-});
     }
 </script>
 </body>
